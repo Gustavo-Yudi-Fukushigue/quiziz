8da48fd1edf94f3edf56598a8139ef18272ac459
Gustavo-Yudi-Fukushigue <gustavoyudifukushigue@gmail.com>
2024-12-14 22:45:10 -0300

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,16 @@
+
+window.revelar = ScrollReveal({reset:true})
+
+//TOPO
+revelar.reveal('.efeito-txt-topo',
+    {
+        duration: 1000,
+        distance: '90px',
+    })
+
+
+//TITULO
+revelar.reveal('.efeito-txt-titulo',{
+    duration: 2000,
+    distance: '90px',
+})
